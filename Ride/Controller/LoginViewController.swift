7e4f026df7b4b021a4648793c1bf8cb9de2992f1
Esraa Gamal <esraa.gamal98@gmail.com>
2020-10-09 23:51:40 +0200

    Login Screen

--- Ride/Controller/LoginViewController.swift
+++ Ride/Controller/LoginViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+   
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBAction func loginButton(_ sender: UIButton) {
     }
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        changeUI()
+        
+        // Do any additional setup after loading the view.
+    }
+    func changeUI(){
+        emailTextfield.keyboardAppearance = .dark
+        emailTextfield.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white ])
+        button.layer.cornerRadius = 5
 
+    }
+    
 }
